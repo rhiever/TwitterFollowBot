7607b44ee978105a64a10db8323e4ba63cc8ac88
Randy Olson <rso@randalolson.com>
2014-01-04 20:23:37 -0500

    Add license text to top of file

--- twitter-follow-bot.py
+++ twitter-follow-bot.py
@@ -1,3 +1,21 @@
+"""
+Copyright 2014 Randal S. Olson
+
+This file is part of the Twitter Follow Bot library.
+
+The Twitter Follow Bot library is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+The Twitter Follow Bot library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with the Twitter
+Follow Bot library. If not, see http://www.gnu.org/licenses/.
+"""
+
 from twitter import Twitter, OAuth, TwitterHTTPError
 
 # put your tokens, keys, secrets, and twitter handle in the following variables
