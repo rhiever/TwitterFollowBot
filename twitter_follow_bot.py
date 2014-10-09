9ce5380217779c36ebe5f0c878a136bc93512768
Paul M Furley <paul@paulfurley.com>
2014-10-09 15:41:24 +0100

    Add a log of who we've followed / unfollowed
    It's a record of who we automatically followed and when. This allows us
    to automatically unfollow after a certain period, if required, and avoid
    pestering them again in the future.
    
    Stored as a plain old CSV.

--- twitter_follow_bot.py
+++ twitter_follow_bot.py
@@ -19,8 +19,18 @@ the Twitter Follow Bot library. If not, see http://www.gnu.org/licenses/.
 
 from __future__ import unicode_literals
 
+import csv
+import datetime
+
+from collections import OrderedDict
+
 from twitter import Twitter, OAuth, TwitterHTTPError
 import os
+from os.path import dirname, join as pjoin
+
+import logging
+logger = logging.getLogger(__name__)
+
 
 API_KEY = os.environ['API_KEY']
 API_SECRET = os.environ['API_SECRET']
@@ -28,12 +38,100 @@ ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
 TWITTER_HANDLE = os.environ['TWITTER_HANDLE']
 
+DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+
 # put the full path and file name of the file you want to store your "already
 # followed" list in
 ALREADY_FOLLOWED_FILE = "already-followed.csv"
 
 t = Twitter(auth=OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET,
             API_KEY, API_SECRET))
+
+
+class FollowLog(object):
+
+    FOLLOW_LOG = pjoin(dirname(__file__), 'follow_log.csv')
+    FOLLOW_LOG_FIELDS = ('twitter_id', 'screen_name', 'follow_datetime',
+                         'unfollow_datetime', 'follow_reason')
+
+    def __init__(self):
+        self._following = OrderedDict()
+
+    @staticmethod
+    def _empty_row(twitter_id):
+        row = {k: None for k in FollowLog.FOLLOW_LOG_FIELDS}
+        row['twitter_id'] = twitter_id
+        return row
+
+    @staticmethod
+    def _serialize_row(row):
+        row['follow_datetime'] = row['follow_datetime'].strftime(
+            DATETIME_FORMAT)
+        return row
+
+    @staticmethod
+    def _deserialize_row(row):
+        row['follow_datetime'] = datetime.datetime.strptime(
+            row['follow_datetime'], DATETIME_FORMAT)
+        return row
+
+    def __enter__(self):
+        self._load_from_csv()
+        return self
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        if exception_type is None:
+            self._save_to_csv()
+
+    def _load_from_csv(self):
+        if not os.path.exists(self.FOLLOW_LOG):
+            self._following = OrderedDict()
+            return
+
+        with open(self.FOLLOW_LOG, 'r') as f:
+            self._following = OrderedDict(
+                [(int(row['twitter_id']), self._deserialize_row(row))
+                 for row in csv.DictReader(f)])
+
+    def _save_to_csv(self):
+        tmp_filename = self.FOLLOW_LOG + '.tmp'
+        with open(tmp_filename, 'w') as f:
+            writer = csv.DictWriter(f, self.FOLLOW_LOG_FIELDS)
+            writer.writeheader()
+            for twitter_id, row in self._following.items():
+                row['twitter_id'] = twitter_id
+                writer.writerow(self._serialize_row(row))
+
+        os.rename(tmp_filename, self.FOLLOW_LOG)
+
+    def _get_or_create(self, twitter_id):
+        if twitter_id not in self._following:
+            self._following[twitter_id] = self._empty_row(twitter_id)
+        return self._following[twitter_id]
+
+    def save_follow(self, twitter_id, reason=None):
+        entry = self._get_or_create(twitter_id)
+
+        entry['follow_datetime'] = datetime.datetime.now()
+        entry['follow_reason'] = reason
+
+    def save_unfollow(self, twitter_id):
+        entry = self._get_or_create(twitter_id)
+        entry['unfollow_datetime'] = datetime.datetime.now()
+
+    def have_followed_before(self, twitter_id):
+        entry = self._following.get(twitter_id)
+        if entry is None:  # no record of this twitter id.
+            return False
+
+        if entry['follow_datetime'] is not None:
+            return True
+
+        return False
+
+
+def get_follow_log():
+    return FollowLog()
 
 
 def search_tweets(q, count=100, result_type="recent"):
@@ -107,15 +205,10 @@ def auto_follow(q, count=100, result_type="recent"):
 
     to_follow -= already_following
     print("Following {} users".format(len(to_follow)))
-    for twitter_id in to_follow:
-        print(twitter_id)
-        try:
-            t.friendships.create(user_id=twitter_id, follow=True)
-        except TwitterHTTPError as e:
-            if 'blocked' not in str(e).lower():  # ignore block errors
-                print(repr(e))
-            else:
-                raise
+    with get_follow_log() as follow_log:
+        for twitter_id in to_follow:
+            _follow(follow_log, twitter_id,
+                    'Tweet: `{}`'.format(tweet['text']))
 
 
 def auto_follow_followers():
@@ -172,3 +265,18 @@ def auto_unfollow_nonfollowers():
         if user_id not in users_keep_following:
             t.friendships.destroy(user_id=user_id)
             print("unfollowed %d" % (user_id))
+
+
+def _follow(follow_log, twitter_id, reason=None):
+    print(twitter_id)
+    try:
+        t.friendships.create(user_id=twitter_id, follow=True)
+    except TwitterHTTPError as e:
+        if 'blocked' in str(e).lower():  # ignore block errors
+            # details: {"errors":[{"code":162,
+            # "message":"You have been blocked from following this account
+            # at the request of the user."}]}
+            logging.info('Ignoring "blocked" exception')
+            logging.exception(e)
+    else:
+        follow_log.save_follow(twitter_id, reason)
